4019bb9e15ba76cbfa599b2ae658af82154e631e
ghoulie-jpg <104750700+ghoulie-jpg@users.noreply.github.com>
2025-07-05 13:48:20 -0400

    changes

--- api/hello.py
+++ api/hello.py
@@ -12,7 +12,6 @@ def main():
 
 
 def parse_content_from_url_news():
-
     url = "https://www.cbc.ca/news/canada/first-person-refugee-small-town-canada-1.7571127"
     tavily_client = TavilyClient(api_key="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")
     response = tavily_client.crawl(url, instructions="Find news content")
