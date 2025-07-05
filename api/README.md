9f20ce51e0fa846043b384a0ae156474adef1305
ghoulie-jpg <104750700+ghoulie-jpg@users.noreply.github.com>
2025-07-05 13:46:19 -0400

    Tavily stuff

---
+++ api/README.md
@@ -0,0 +1,3 @@
+notes on biased vs unbiased news media : 
+https://aml.ca/the-bias-in-media-bias-charts/
+
--- api/hello.py
+++ api/hello.py
@@ -1,11 +1,24 @@
-"""
-FastAPI app for the API powered by Tavily.
-"""
+from tavily import TavilyClient
 
-
+# make a call 
+# LLM to get list of facts
+# LLM queries Tavily
+# Tavily will return a list of 
+# use SQL Lite 
+# False, True, Somewhat True : With the sentence
 def main():
     print("Hello from api!")
+    parse_content_from_url_news()
 
+
+def parse_content_from_url_news():
+
+    url = "https://www.cbc.ca/news/canada/first-person-refugee-small-town-canada-1.7571127"
+    tavily_client = TavilyClient(api_key="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")
+    response = tavily_client.crawl(url, instructions="Find news content")
+    print(response)
+
+    
 
 if __name__ == "__main__":
     main()
